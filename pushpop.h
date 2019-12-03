b003dda67670baf321207f02d7fba8acc9005612
ratihindah1006 <48676219+ratihindah1006@users.noreply.github.com>
2019-12-03 10:57:40 +0700

    Update pushpop.h

--- pushpop.h
+++ pushpop.h
@@ -6,57 +6,68 @@ const int batas = 3;
 int antrian[batas];
 int data = -1;
 
-bool isEmpty(){ 
-	if(data == -1){
+bool isEmpty()
+{ 
+	if(data == -1)
+	{
 		return true;
-		}
-	return false;
 	}
+		return false;
+}
 
 bool isFull()
-	{ 
-	if(data >= 2){
+{ 
+	if(data >= 2)
+	{
 		return true;
-		}
-	return false;
-	}		
+	}
+		return false;
+}		
 		 
 void push(long int x)
-	{	
-	if(isFull()){
+{	
+	if(isFull())
+	{
 		output<<"sudah mencapai maksimal mahasiswa yang mengambil mata kuliah inputi"<<enter;
-		}
-	else{
+	}
+	else
+	{
 		data++;
 		antrian[data] = x;
 			output<<"anda berhasil mendapat nomor antrian UAS"<<enter;
-		}	
-	}
+	}	
+}
 	
-void pop(){		
-	if(isEmpty()){
+void pop()
+{		
+	if(isEmpty())
+	{
 		output<<"Tidak ada yang mengantri untuk ujian"<<enter;
-		}
-	else{
+	}
+	else
+	{
 		output<<antrian[0]<<" diharapkan memasuki ruang ujian"<<enter;
-		for(int i=0 ; i<=data ; i++){
+		for(int i=0 ; i<=data ; i++)
+		{
 			antrian[i] = antrian[i+1];
 		}
 		data--;
-		}
 	}
+}
 	
-void tampil(){
-	if(isEmpty()){
+void tampil()
+{
+	if(isEmpty())
+	{
 		output<<"tidak ada antrian"<<enter;
+	}
+	else
+	{
+		for(int i=0 ; i<=data ; i++)
+		{
+			output<<antrian[i]<<" ";
 		}
-	else{
-		for(int i=0 ; i<=data ; i++){
-		output<<antrian[i]<<" ";
-			}
 		output<<"\nTotal antrian UAS = "<<data+1<<enter;
-		}
-	
 	}
 	
-
+}
