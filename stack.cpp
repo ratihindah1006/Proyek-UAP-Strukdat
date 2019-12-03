32cbe63af970df5e6e80d53766b90cda74e95522
ratihindah1006 <48676219+ratihindah1006@users.noreply.github.com>
2019-12-03 10:08:36 +0700

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include"pushpop.h"
+#include "kamus.h"
+using namespace std;
+
+int nama();
+
+int main()
+{
+	int pilih, tanda=1, menu;
+	long int npm;
+	menu = nama();
+
+ while( tanda == 1){
+
+output<<enter<<enter<<enter;
+   
+output<<"===================\n";
+output<<"|     PILIHAN     |\n";
+output<<"===================\n";
+output<<"1. Push \n";
+output<<"2. Pop \n";
+output<<"3. Tampilkan\n";
+output<<"4. exit \n";
+output<<"===================\n";
+   
+output<<"Pilih : "; input>>pilih;
+
+	switch (pilih)
+	{
+	case 1: output<<"Masukan NPM anda : ";
+			input>>npm;
+			if (npm == 1867051001){
+				output<<"\nNPM yanng anda masukkan berhasil terdaftar"<<enter;
+				push(npm);
+			}
+			else if (npm <= 1817051075 && npm >=1817051001)
+			{
+				output<<"\nNPM yanng anda masukkan berhasil terdaftar"<<enter;
+				push(npm);
+			}
+			else if (npm <= 1857051015 && npm >=1857051001)
+			{
+				output<<"\nNPM yanng anda masukkan berhasil terdaftar"<<enter;
+				push(npm);
+			}
+			else {
+				output<<"\nNPM yang anda masukkan salah";
+			}	
+			break;
+			
+	case 2: pop();
+			break;
+			
+	case 3: tampil();		
+			break;
+			
+	case 4: tanda = 0;
+			output << enter << enter;
+			break;
+			
+	default:
+			output <<"\t\nPilihan tidak ada";
+			input.get();
+			break;
+         
+ 	}
+ }
+    return 0;
+}
+int nama()
+{
+output<<"\t\t============================================"<<enter;
+output<<"\t\t|                                           |"<<enter;
+output<<"\t\t|          PROGRAM DIBUAT OLEH :            |"<<enter;
+output<<"\t\t|          RATIH inputDAH WARDANI              |"<<enter;
+output<<"\t\t|          RAHMADILA NURJANNAH              |"<<enter;
+output<<"\t\t|          DEWI LESTARI                     |"<<enter;
+output<<"\t\t|                                           |"<<enter;
+output<<"\t\t============================================"<<enter<<enter;
+
+output<<"Deskripsi Program :"<<enter;
+output<<"Program inputi dibuat untuk menentukan urutan UAS lisan mahasiswa S1 Ilmu Komputer 2018"<<enter;
+}
